b54867b9b1c45205624f5f89fb7b15babc4206a2
Y. Violet Guo <yueguo598@gmail.com>
2020-05-05 11:42:38 -0300

    can clear and reset, but hover not working

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // functions
-function draw_squares(eachSide=5) {
+function draw_squares(eachSide) {
     var container = document.querySelector('.container');
     //  container.innerHTML = '';//don't want any extra boxes when you call this function again
     
@@ -17,10 +17,12 @@ function draw_squares(eachSide=5) {
     } 
 }
 
-function define_style(box, colour){
-    let boxLength = 50;    
+function define_style(box, colour, eachSide){
+    let boxLength =  document.documentElement.clientHeight / eachSide - 4;
+
     box.style.width = Math.floor(boxLength);
     box.style.height = Math.floor(boxLength)
+
     box.style.backgroundColor = colour;
     box.style.padding = 4;
     box.style.border= '4px solid #fff';//border for margin but use border-box to make sure the width and height are still 50px
@@ -29,42 +31,50 @@ function define_style(box, colour){
 
 }
 
-function hoverOver(boxes){
+function hoverOver(boxes, eachSide){
     boxes.forEach(function (box){
         box.addEventListener('mouseenter', function() {
-            define_style(box, 'white');
+            define_style(box, 'white', eachSide);
         });
     });
 }
 
-function clear_squares(outBoxes, eachSide=5) {
+function clear_squares(eachSide) {
     const boxes = document.querySelectorAll('.box');
     //  container.innerHTML = '';//don't want any extra boxes when you call this function again
-    console.log(boxes);
-    
-    boxes.forEach(function (box) {define_style(box, 'blue')});
+    boxes.forEach(function (box) {define_style(box, 'blue', eachSide)});
     
   }
 
+function clear_button(eachSide){
+    const btn = document.querySelector("#clear");
+    btn.addEventListener('click', function() {
+        clear_squares(eachSide);
+        let num = prompt("New grid dimension: ");
 
-function loadWindow(){
+        // delete the boxes
+        document.querySelectorAll(".box").forEach(e => e.parentNode.removeChild(e));
+        draw_squares(num);
+    });
+    
+}
+
+function loadWindow(eachSide){
     window.addEventListener('load', function() {
-        draw_squares();
+        draw_squares(eachSide);
         outBoxes = document.querySelectorAll('.box');
         // change colour to white when you hover over 
-        hoverOver(outBoxes);
-        clear(outBoxes);
+        hoverOver(outBoxes, eachSide);
+        clear_button(eachSide);
     });
 }
 
-function clear(outBoxes){
-    const btn = document.querySelector("#clear");
-    btn.addEventListener('click', function() {clear_squares();});
-}
+
 
 // main script
 let outBoxes;
-loadWindow();
+const defaultSide = 5;
+loadWindow(defaultSide);
 
 
 
